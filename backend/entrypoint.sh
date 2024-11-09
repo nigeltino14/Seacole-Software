1e54efd48f28128ef401afd260acb550f7f51b06
Tawanda Muzanenhamo <tawandamuzanenhamo14@gmail.com>
2024-11-09 12:22:32 +0200

    test different server settings

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
-
-
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+exec python manage.py runserver 0.0.0.0:8000
